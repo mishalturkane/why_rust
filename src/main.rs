2106a20183b43ba2e0a784ac999b7a7ff6fd5637
mishalturkane <mishalturkane@gmail.com>
2026-06-02 11:52:22 +0530

    fix: fixed some codeover here

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use why_rust::models::two_sum::*; 
-
-fn main() {
-    println!("Result: {}", two_sum(4, 5));
-    println!("Result: {}", two_sum(4, 55));
+fn main(){
+    println!("hello ji");
 }
--- src/models/two_sum.rs
+++
@@ -1,3 +0,0 @@
- pub fn two_sum(x: i32, y: i32) -> i32 {
-     x + y  // `return` is optional in Rust
- }
